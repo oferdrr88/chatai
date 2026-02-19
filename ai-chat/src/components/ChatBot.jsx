51c9f11fa9f51b88ecb2c9a9fc403d83010fc42d
oferdrr88 <oferdrr8@gmail.com>
2026-02-19 13:20:06 +0200

    css start-page

---
+++ ai-chat/src/components/ChatBot.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ChatBot.css'
+
+const ChatBot = () => {
+  return (
+    <div className='start-page'>
+        <button className="start-page-btn">CHAT AI</button>
+      
+    </div>
+  )
+}
+
+export default ChatBot
